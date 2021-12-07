7fed41d0fd0c3316ef81f9b1f672a09613197bb8
Evgeny Pavlov <epavlov@mozilla.com>
2021-12-07 11:50:28 -0800

    Pivot translation (#9)

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+curl http://0.0.0.0:8080/status
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"from":"en", "to":"es", "text": "Hello world"}' \
+  http://0.0.0.0:8080/v1/translate
+
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"from":"es", "to":"en", "text": "Hola Mundo"}' \
+  http://0.0.0.0:8080/v1/translate
+
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"from":"es", "to":"et", "text": "Hola Mundo"}' \
+  http://0.0.0.0:8080/v1/translate
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"from":"xx", "to":"yy", "text": "Hola Mundo"}' \
+  http://0.0.0.0:8080/v1/translate
