2bc01c0fd3f5113689d726b9ca0566846c1f14c0
evgeny pavlov <epavlov@mozilla.com>
2021-12-09 17:08:17 -0800

    Fix utf encoding in json response

--- src/server.cpp
+++ src/server.cpp
@@ -6,9 +6,23 @@
 #include <sstream>
 #include <map>
 #include <filesystem>
+#include <iomanip>
 
 #include "translation.h"
 #include "crow.h"
+
+std::string escape_json(const std::string &s) {
+    std::ostringstream o;
+    for (auto c = s.cbegin(); c != s.cend(); c++) {
+        if (*c == '"' || *c == '\\' || ('\x00' <= *c && *c <= '\x1f')) {
+            o << "\\u"
+              << std::hex << std::setw(4) << std::setfill('0') << (int)*c;
+        } else {
+            o << *c;
+        }
+    }
+    return o.str();
+}
 
 void run(int port, int workers, crow::LogLevel logLevel) {
     marian::bergamot::TranslatorWrapper wrapper(workers);
@@ -37,9 +51,11 @@ void run(int port, int workers, crow::LogLevel logLevel) {
                         auto result = wrapper.translate(from, to, input);
                         CROW_LOG_DEBUG << "Finished translation from " << from << " to " << to << ": " << result;
 
-                        crow::json::wvalue jsonRes;
-                        jsonRes["result"] = result;
-                        return crow::response(jsonRes);
+                        // crow json escaping messes up utf-8
+                        auto json_result = "{\"result\": \"" + escape_json(result) + "\"}";
+                        auto response = crow::response(json_result);
+                        response.set_header("Content-Type", "application/json; charset=utf-8");
+                        return response;
                     });
 
     CROW_ROUTE(app, "/__heartbeat__")
