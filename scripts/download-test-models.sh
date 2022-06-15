0ea9a31476df22e31f829e17f211d49ace175ee3
evgeny pavlov <epavlov@mozilla.com>
2022-06-15 16:00:33 -0700

    Add test model downloading

--- scripts/download-test-models.sh
+++ scripts/download-test-models.sh
@@ -6,19 +6,26 @@ cd models
 mkdir esen
 mkdir enet
 mkdir enru
+mkdir enuk
 
 cd esen
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/esen/lex.50.50.esen.s2t.bin
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/esen/model.esen.intgemm.alphas.bin
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/esen/vocab.esen.spm
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/esen/lex.50.50.esen.s2t.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/esen/model.esen.intgemm.alphas.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/esen/vocab.esen.spm
 
 cd ../enet
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/enet/lex.50.50.enet.s2t.bin
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/enet/model.enet.intgemm.alphas.bin
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/enet/vocab.eten.spm
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enet/lex.50.50.enet.s2t.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enet/model.enet.intgemm.alphas.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enet/vocab.eten.spm
 
 
 cd ../enru
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/enru/lex.50.50.enru.s2t.bin
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/enru/model.enru.intgemm.alphas.bin
-wget https://storage.googleapis.com/bergamot-models-sandbox/0.2.10/enru/vocab.enru.spm
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enru/lex.50.50.enru.s2t.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enru/model.enru.intgemm.alphas.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enru/vocab.enru.spm
+
+cd ../enuk
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enuk/lex.50.50.enuk.s2t.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enuk/model.enuk.intgemm8.bin
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enuk/srcvocab.enuk.spm
+wget https://storage.googleapis.com/bergamot-models-sandbox/0.3.0/enuk/trgvocab.enuk.spm
